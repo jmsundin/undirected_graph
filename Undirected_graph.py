260b9dfb9db381dd0d000055430de226ac0ca7b9
Jon Sundin <jsundin91@gmail.com>
2022-04-21 22:24:10 -0700

    added plot_graph func

--- Undirected_graph.py
+++ Undirected_graph.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 import networkx as nx
+import matplotlib.pyplot as plt
 
 
 class Undirected_graph():
@@ -8,7 +9,14 @@ class Undirected_graph():
     def __init__(self) -> None:
         pass
 
-    
+
+    def plot_graph(G, node_labels):
+        plt.subplot(1, 1, 1)
+        pos = nx.spring_layout(G, k=0.15, seed=42) (1)
+        nx.draw_networkx(G, pos=pos, with_labels=True, labels=node_labels, font_weight='bold')
+        plt.show()
+
+
     def create_undirected_graph(noun_chunks):
         similarity_matrix = np.triu(similarity_matrix, k=1)
         iterator = np.nditer(similarity_matrix, flags=['multi_index'], order='C')
@@ -25,3 +33,6 @@ class Undirected_graph():
                     node_labels[key] = value
                 G.add_node(iterator.multi_index[0])
                 G.add_edge(iterator.multi_index[0], iterator.multi_index[1], weight=edge)
+
+                Undirected_graph.plot_graph(G, node_labels)
+
