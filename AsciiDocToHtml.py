d6f50308931d72fa52154de63eff5a8b7e3d4c18
Jon Sundin <jsundin91@gmail.com>
2022-04-21 20:57:44 -0700

    created instance variable for html_file_name

--- AsciiDocToHtml.py
+++ AsciiDocToHtml.py
@@ -1,29 +1,26 @@
 import subprocess
 import os
 import sys # to read in stdin
-import re
+
 from bs4 import BeautifulSoup
 
 
-NLPIA_Ch6_my_section = 'NLPIA_Ch6_my_section.adoc'
-NLPIA_Ch6_my_section_html = 'NLPIA_Ch6_my_section.html'
-
 class AsciiDocToHtml():
     
-    def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
+    def __init__(self, adoc_file_name: str, html_file_name: str) -> None:
+        self.adoc_file_name = adoc_file_name
+        self.html_file_name = html_file_name
 
     
     def run_ascii_doc3(self) -> None:
-        subprocess.run(args=['asciidoc3', '-a', '-n', '-a', 'icons', self.file_name])
-    
+        subprocess.run(args=['asciidoc3', '-a', '-n', '-a', 'icons', self.adoc_file_name])
 
-    def run_beautiful_soup(self):
 
-        if os.path.exists() and os.path.getsize(NLPIA_Ch6_my_section_html) > 0:
-            f = open(NLPIA_Ch6_my_section_html, 'r').read()
+    def run_beautiful_soup(self) -> str:
+        if os.path.exists(self.html_file_name) and os.path.getsize(self.html_file_name) > 0:
+            f = open(self.html_file_name, 'r').read()
             bsoup = BeautifulSoup(f, 'html.parser')
-            text = bsoup.get_text()
+            return bsoup.get_text()
 
 
 if __name__ == "__main__":
@@ -34,4 +31,7 @@ if __name__ == "__main__":
         ascii_doc_file = line.rstrip()
 
     if 'adoc' in ascii_doc_file:
-        AsciiDocToHtml(ascii_doc_file)
+        NLPIA_Ch6_my_section_html = ascii_doc_file.replace('adoc', 'html')
+        AsciiDocToHtml(ascii_doc_file, NLPIA_Ch6_my_section_html)
+        AsciiDocToHtml.run_ascii_doc3()
+        AsciiDocToHtml.run_beautiful_soup()
